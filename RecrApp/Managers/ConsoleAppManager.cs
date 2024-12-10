597be49bd88ca87e173675949074fe2ed4573222
Manno-al-mano <83082775+Manno-al-mano@users.noreply.github.com>
2024-12-10 17:16:18 +0100

    +Added ConsoleAppManager class that will manage interactions with console

---
+++ RecrApp/Managers/ConsoleAppManager.cs
@@ -0,0 +1,35 @@
+﻿using RecrApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecrApp.Managers
+{
+    internal class ConsoleAppManager
+    {
+        private Order _order;
+        private Product _laptop;
+        private Product _keyboard;
+        private Product _mouse;
+        private Product _monitor;
+        private Product _duck;
+
+        public ConsoleAppManager()
+        {
+            _order = new Order();
+            _laptop= new Product("Laptop",2500);
+            _keyboard = new Product("Klawiatura", 120);
+            _mouse = new Product("Mysz", 90);
+            _monitor = new Product("Monitor", 1000);
+            _duck = new Product("Kaczka debuggująca", 66);
+        }
+
+        public void Start()
+        {
+
+        }
+
+    }
+}
