be8f95ddf1d8cc4019e4dc0834c086602d942d25
Manno-al-mano <83082775+Manno-al-mano@users.noreply.github.com>
2024-12-10 16:34:37 +0100

    +Added Ordered item list to Order +Added methods to adding and removing products, as well as calculating total price with discounts

--- RecrApp/Models/Order.cs
+++ RecrApp/Models/Order.cs
@@ -8,5 +8,51 @@ namespace RecrApp.Models
 {
     internal class Order
     {
+        private List<OrderItem> _items = new List<OrderItem>();
+        public void AddItem(Product product, int quantity)
+        {
+            var existingItem = _items.FirstOrDefault(item => item.Product.Name == product.Name);
+            if (existingItem != null)
+            {
+                existingItem.Quantity += quantity;
+            }
+            else
+            {
+                _items.Add(new OrderItem(product, quantity));
+            }
+        }
+
+        public void RemoveItem(Product product)
+        {
+            var existingItem = _items.FirstOrDefault(item => item.Product.Name == product.Name);
+            if (existingItem != null)
+            {
+                _items.Remove(existingItem);
+            }
+        }
+
+        public decimal GetTotalValue()
+        {
+            var products = _items
+    .SelectMany(item => Enumerable.Repeat(item.Product.Price, item.Quantity))
+    .OrderBy(p => p)
+    .ToList();
+            decimal total = products.Sum();
+            if (products.Count > 3)
+            {
+                total -= products[0] * 0.1M;
+            }
+            else if (products.Count == 2)
+            {
+                total -= products[0] * 0.2M;
+            }
+            if (total > 5000)
+            {
+                return total * .95M;
+            }
+            return total;
+        }
     }
+
+
 }
