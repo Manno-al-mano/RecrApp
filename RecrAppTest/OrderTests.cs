864e6373926d6bb6be4ac78ca856f6229a6557ca
Manno-al-mano <83082775+Manno-al-mano@users.noreply.github.com>
2024-12-10 20:26:10 +0100

    +Created Test Project =Moved Test classes to new Test Project =Ensured Test SDK and XUnit packages are installed and uninstalled correctly

---
+++ RecrAppTest/OrderTests.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+using RecrApp.Models;
+
+namespace RecrAppTest
+{
+    public class OrderTests
+    {
+        
+           [Theory]
+          [InlineData(1, 100, 100)]
+          [InlineData(2, 100, 190)]
+          [InlineData(3, 100, 280)]
+          [InlineData(1, 10000, 9500)]
+          [InlineData(2, 5000, 9025)]
+          [InlineData(4, 5000, 13300)]
+          public void TestDiscounts(int numberOfProducts, decimal price, decimal expectedPrice)
+          {
+
+                   Order order = new Order();
+                   for (int i = 0; i < numberOfProducts; i++)
+                   {
+                       string name = "Product" + i;
+                       Product product = new Product(name, price);
+                       order.AddItem(product, 1);
+                   }
+                   var discountedValue = order.GetTotalDiscountedValue();
+                   Assert.Equal(expectedPrice, discountedValue); 
+                   }
+    }
+}
